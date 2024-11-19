d59b36d8fd4f43f47a5da20ff5ef843e642e8941
i-piiyush <piyushchhabra3578@gmail.com>
2024-11-19 19:41:13 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -8,40 +8,39 @@ gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-
   useGSAP(() => {
-    gsap.from("#box",{
-      opacity:0,
-      y:40,
-      duration:1
-    })
- 
-   gsap.from("#box2",{
-    filter:"blur(30px)",
-    opacity:0,
-    scrollTrigger:{
-      trigger:"#box2",
-      scroller:"body",
-      markers:true,
-      start:"top 50%",
-      scrub:true,
-      end:"bottom 30%"
-    }
-   })
+    gsap.from("#box", {
+      opacity: 0,
+      y: 40,
+      duration: 1,
+    });
+
+    gsap.to(".page2 h1", {
+      transform: "translateX(-30%)",
+      scrollTrigger: {
+        trigger: ".page2",
+        scroller:"main",
+        scrub: 3,
+        markers: true,
+        start: "top 10%",
+        end: "top -150%",
+        pin: true,
+      },
+    });
   });
 
   return (
     <>
-      <main>
-        <div className="page1 ">
-         <h1 id="box">hii ji</h1>
-        </div>
-        <div className="page2">
-          
-          <h1 id="box2">kese Ho?</h1>
-        </div>
-        <div className="page1"></div>
-      </main>
+    <main>
+       <div className="page1 ">
+        <h1 id="box">hii ji</h1>
+      </div>
+      <div className="page2">
+        <h1>kese Ho?</h1>
+      </div>
+      <div className="page1"></div>
+    </main>
+     
     </>
   );
 }
